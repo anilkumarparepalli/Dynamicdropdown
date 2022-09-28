38774741ac4a40241ce03fa8acb668a7428a670c
anilkumarparepalli <114554732+anilkumarparepalli@users.noreply.github.com>
2022-09-28 10:22:00 +0530

    Add files via upload

---
+++ Dynamicdropdown/src/main/java/com/example/Dynamicdropdown/DynamicdropdownApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dynamicdropdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicdropdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicdropdownApplication.class, args);
+	}
+
+}
